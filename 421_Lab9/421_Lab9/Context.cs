ced3fe1fa3984bcdb7766a5f9caff978458ef4b4
Andrew Roseman <whoisroseman@gmail.com>
2018-03-29 13:32:52 -0400

    Lab 9 Changes
    Refactored state architecture and began DoIt() implementation.

---
+++ 421_Lab9/421_Lab9/Context.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace _421_Lab9
+{
+    public class Context
+    {
+        public IState State { get; set; }
+
+        public Context()
+        {
+            State = new Value1();
+            
+        }
+
+        public void DoIt(Button button)
+        {
+            State.DoIt(this, button);
+        }
+    }
+}
--- 421_Lab9/421_Lab9/Value1.cs
+++ 421_Lab9/421_Lab9/Value1.cs
@@ -1,15 +1,28 @@
-﻿namespace _421_Lab9
-{
-    public class Value1:State
-    {
-        public override State NextState(State s)
-        {
-            throw new System.NotImplementedException();
-        }
+﻿using System.Windows.Forms;
 
-        public override void DoIt()
+namespace _421_Lab9
+{
+    public class Value1:IState
+    {
+        public void DoIt(Context context, char action)
         {
-            throw new System.NotImplementedException();
+            // Check if the action is 0-9
+            if (action >= 48 && action <= 57)
+            {
+                // Do some action here and then transtion.
+                context.State = new Value1();
+            }
+            // Check if the action is a basic operation ( +, -, *, ., /)
+            else if ((action >= 42 && action <= 43) || (action >= 45 && action <= 47))
+            {
+                // Do some action here and then transition.
+                context.State = new OperatorEntered();
+            }
+            // Special Operations (sqrt, 1/x, = etc...)
+            else if (action == 251)
+            {
+
+            }
         }
     }
 }
