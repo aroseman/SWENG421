7d0659ab3bfa9684facb67a57351bd5dfdc3979f
Unknown <connortkd@gmail.com>
2018-05-02 17:37:12 -0400

    Added Wrapper
    I think I added in wrapper well. We just have to make sure the constructor is called the appropriate amount of times.

---
+++ HospitalSystem/HospitalSystem/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem
+{
+    class Admin : AdminThingIF
+    {
+        public AdminThingIF Enroll(string mystring)
+        {
+            return new Admin();
+        }
+    }
+}
---
+++ HospitalSystem/HospitalSystem/VitalWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem
+{
+    class VitalWrapper : VitalsIF
+    {
+        public VitalsIF lastVital;
+        public VitalWrapper myWrapped;
+        //holds the most recently added vital
+
+        public VitalWrapper(VitalWrapper oldWrapped, VitalWrapper newVital)
+        {
+            // VitalWrapper temp = new HospitalSystem.VitalWrapper();
+            oldWrapped.lastVital = newVital;
+            oldWrapped.myWrapped = oldWrapped;
+
+        }
+        public VitalWrapper()
+        {
+        }
+    }
+}
